9f281758b9cd255cef6b2b6b71cb2f4fc4206f28
Kamakshya Nanda <91257779+kamakshyan@users.noreply.github.com>
2026-03-08 05:05:22 -0500

    task: test driver update

--- main.cpp
+++ main.cpp
@@ -1,28 +1,17 @@
-#include <iostream>
+#include "logger.h"
+#include "console_sink.h"
+#include <memory>
 
-class Logger{
-    public:
-        void log(const std::string& message){
-            std::cout << message << std::endl;
-        };
-
-        void info(const std::string& message){
-            log("INFO: " + message);
-        };
-
-        void warn(const std::string& message){
-            log("WARN: " + message);
-        };
-
-        void error(const std::string& message){
-            log("ERROR: " + message);
-        };
-};
 int main(){
     Logger logger;
-    logger.log("Logger Called!");
-    logger.info("Info message");
-    logger.warn("Warn message");
-    logger.error("Error message");
+
+    logger.addSink(std::make_shared<ConsoleSink>());
+
+    logger.info("Info Message");
+    logger.warn("Warning Message");
+    logger.error("Error Message");
+
+    logger.log("Logger Logged");
+
     return 0;
 }
